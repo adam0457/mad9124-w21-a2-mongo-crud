b8e78007f118b9a158a1dce13f0b441d3bd4d3c0
adam0457 <adam0457@algonquinlive.com>
2022-03-05 16:41:37 -0500

    Add the GET route logic for the courses

---
+++ routes/courses.js
@@ -0,0 +1,105 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const Course = require('../models/Course')
+
+router.get('/', async (req, res) => {
+  const courses = await Course.find()
+  res.json({data: courses.map(course => formatResponseData('courses', course.toObject()))})
+})
+
+router.post('/', async (req, res) => {
+  let attributes = req.body.data.attributes
+  delete attributes._id // if it exists
+
+  let newStudent = new Student(attributes)
+  await newStudent.save()
+
+  res.status(201).json({data: formatResponseData('students', newStudent.toObject())})
+})
+
+router.get('/:id', async (req, res) => {
+  console.log(req.params.id)
+    try {
+      const student = await Student.findById(req.params.id)//.populate('owner')
+      if(!student){
+          throw new Error('Resource not found')
+      }
+      res.json({data: formatResponseData('students', student.toObject())})
+    }catch(err) {
+      sendResourceNotFound(req, res)
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+      const {_id, ...otherAttributes} = req.body.data.attributes
+      const student = await Student.findByIdAndUpdate(
+        req.params.id,
+        {_id: req.params.id, ...otherAttributes},
+        {
+          new: true,
+          runValidators: true
+        }
+      )
+      if (!student) throw new Error('Resource not found')
+      res.json({data: formatResponseData('students', student.toObject())})
+    } catch (err) {
+      sendResourceNotFound(req, res)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+      const {_id, ...otherAttributes} = req.body.data.attributes
+      const student = await Student.findByIdAndUpdate(
+        req.params.id,
+        {_id: req.params.id, ...otherAttributes},
+        {
+          new: true,
+          overwrite: true,
+          runValidators: true
+        }
+      )
+      if (!student) throw new Error('Resource not found')
+      res.json({data: formatResponseData('students', student.toObject())})
+    } catch (err) {
+      sendResourceNotFound(req, res)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+      const student = await Student.findByIdAndRemove(req.params.id)
+      if (!student) throw new Error('Resource not found')
+      res.json({data: formatResponseData('students', student.toObject())})
+    } catch (err) {
+      sendResourceNotFound(req, res)
+    }
+})
+
+/**
+ * Format the response data object according to JSON:API v1.0
+ * @param {string} type The resource collection name, e.g. 'cars'
+ * @param {Object} resource An instance object from that collection
+ * @returns
+ */
+function formatResponseData(type, resource) {
+  const {id, ...attributes} = resource
+  return {type, id, attributes}
+}
+
+function sendResourceNotFound(req, res){
+  res.status(404).send({
+    errors: [
+      {
+        status: '404',
+        title: 'Resource does not exist',
+        description: `We could not find a student with id: ${req.params.id}`
+      }
+    ]
+  })
+}
+
+module.exports = router
