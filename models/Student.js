991b46f230b3ee8b6041e704196d7ac555a97646
adam0457 <adam0457@algonquinlive.com>
2022-03-05 13:29:22 -0500

    Add the mongoose model for Student

---
+++ models/Student.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  firstName: {type: String, maxlength: 64, required: true},
+  lastName: {type: String, maxlength: 64, required: true},
+  nickName: {type: String, maxlength: 64, required: false},
+  email: {type: String, maxlength: 512, required: true}  
+})
+const Model = mongoose.model('Student', schema)
+
+module.exports = Model
