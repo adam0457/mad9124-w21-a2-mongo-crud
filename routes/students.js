081bfc26769d62f053084c553b92dbd0c79d0ecf
adam0457 <adam0457@algonquinlive.com>
2022-03-05 16:16:23 -0500

    Add the put route logic for the students

--- routes/students.js
+++ routes/students.js
@@ -50,7 +50,24 @@ router.patch('/:id', async (req, res) => {
     }
 })
 
-router.put('/:id', async (req, res) => {})
+router.put('/:id', async (req, res) => {
+    try {
+      const {_id, ...otherAttributes} = req.body.data.attributes
+      const student = await Student.findByIdAndUpdate(
+        req.params.id,
+        {_id: req.params.id, ...otherAttributes},
+        {
+          new: true,
+          overwrite: true,
+          runValidators: true
+        }
+      )
+      if (!student) throw new Error('Resource not found')
+      res.json({data: formatResponseData('students', student.toObject())})
+    } catch (err) {
+      sendResourceNotFound(req, res)
+    }
+})
 
 router.delete('/:id', async (req, res) => {})
 
