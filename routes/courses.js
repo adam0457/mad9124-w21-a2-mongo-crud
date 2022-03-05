136950f70b3ea44f1cc61819531d22db3e9e768a
adam0457 <adam0457@algonquinlive.com>
2022-03-05 17:18:52 -0500

    Add the PATCH route logic for the courses

--- routes/courses.js
+++ routes/courses.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
 router.patch('/:id', async (req, res) => {
     try {
       const {_id, ...otherAttributes} = req.body.data.attributes
-      const student = await Student.findByIdAndUpdate(
+      const course = await Course.findByIdAndUpdate(
         req.params.id,
         {_id: req.params.id, ...otherAttributes},
         {
@@ -43,8 +43,8 @@ router.patch('/:id', async (req, res) => {
           runValidators: true
         }
       )
-      if (!student) throw new Error('Resource not found')
-      res.json({data: formatResponseData('students', student.toObject())})
+      if (!course) throw new Error('Resource not found')
+      res.json({data: formatResponseData('courses', course.toObject())})
     } catch (err) {
       sendResourceNotFound(req, res)
     }
