db49657304455dcfa7e17d7b314894c3417403b6
adam0457 <adam0457@algonquinlive.com>
2022-03-05 16:34:38 -0500

    Create the Course model

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const app = express()
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use('/api/students', require('./routes/students'))
+app.use('/api/courses', require('./routes/courses'))
 
 const port = process.env.PORT || 3030
 app.listen(port, () => console.log(`HTTP server listening on port ${port} ...`))
