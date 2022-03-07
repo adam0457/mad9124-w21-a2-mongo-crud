5d0a5a01daf05c037079254c7908db8c28e57cc6
adam0457 <adam0457@algonquinlive.com>
2022-03-06 21:16:46 -0500

    Add the express mongo sanitize to prevent query injection

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
 const morgan = require('morgan')
 const express = require('express')
 const app = express()
+const sanitizeMongo = require('express-mongo-sanitize')
 require('./startup/database')
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(sanitizeMongo())
 app.use('/api/students', require('./routes/students'))
 app.use('/api/courses', require('./routes/courses'))
 
